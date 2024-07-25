8acdc4cc2613a5830ffdb60e5822bff02552a843
Jaewon Noh <opqw987@gmail.com>
2024-07-25 11:43:08 +0900

    ✨ 헤더 / 푸터 / 공용 레이아웃 추가 (#23)
    * ✨ Header / Footer를 사용한 기본 Layout 구성
    
    * 🎨 기본 페이지 랜딩 페이지로 수정

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "../components/common/DefaultLayout";
+
+const LandingPage = () => {
+  return (
+    <DefaultLayout>
+      <h1>!! Landing !!</h1>
+    </DefaultLayout>
+  );
+};
+
+export default LandingPage;
