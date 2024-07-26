46936d097e8855ed3eeea0bdbdc504a5c3abf64d
Jaewon Noh <opqw987@gmail.com>
2024-07-26 11:18:14 +0900

    💄 랜딩 페이지 추가 (#24)
    * 🚚 헤더/푸터 폴더 분리
    
    * 💄 랜딩 페이지 ui 추가

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,27 @@
+import DefaultGrid from "../components/common/DefaultGrid";
 import DefaultLayout from "../components/common/DefaultLayout";
+import { Hero } from "../components/landing/Hero";
+import { PromotionWineCard } from "../components/promotion/PromotionWineCard";
+import ReviewCard from "../components/review/ReviewCard";
 
 const LandingPage = () => {
   return (
     <DefaultLayout>
-      <h1>!! Landing !!</h1>
+      <Hero />
+      <div style={{ margin: "50px 20px 20px 20px" }}>
+        <DefaultGrid>
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+        </DefaultGrid>
+      </div>
+      <div style={{ margin: "50px 20px 20px 20px" }}>
+        <DefaultGrid>
+          <PromotionWineCard />
+          <PromotionWineCard />
+          <PromotionWineCard />
+        </DefaultGrid>
+      </div>
     </DefaultLayout>
   );
 };
