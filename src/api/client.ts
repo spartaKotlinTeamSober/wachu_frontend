aa802e1548d653eaedeccef9c4947af9cfcc583a
Jaewon Noh <opqw987@gmail.com>
2024-07-29 17:05:57 +0900

    ✨ 배열 query param 제대로 처리

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,11 @@
+import qs from "qs";
 import axios, { AxiosInstance } from "axios";
 
 export const wachuApiClient: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT as string,
+  paramsSerializer: (params) => {
+    return qs.stringify(params, { arrayFormat: "repeat" });
+  },
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
