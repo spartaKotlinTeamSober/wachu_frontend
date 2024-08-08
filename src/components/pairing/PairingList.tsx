c94393420ed5a6ac0de2e1f1be185dff7eeab9b2
Jaewon Noh <opqw987@gmail.com>
2024-08-08 15:22:20 +0900

    ✨ 페어링 카드 리스트 추가

--- src/components/pairing/PairingList.tsx
+++ src/components/pairing/PairingList.tsx
@@ -1,12 +1,19 @@
 import { Pairing } from "../../models/Pairing";
+import DefaultGrid from "../common/DefaultGrid";
+import PairingCard from "./PairingCard";
 
 const PairingList = ({ pairings }: { pairings: Pairing[] }) => {
-  return pairings.map((pairing) => (
-    <div key={pairing.id}>
-      <h2>{pairing.title}</h2>
-      <p>{pairing.description}</p>
+  return (
+    <div style={{ width: "90%", margin: "30px auto" }}>
+      <DefaultGrid>
+        {pairings.map((pairing) => (
+          <div key={pairing.id}>
+            <PairingCard pairing={pairing} />
+          </div>
+        ))}
+      </DefaultGrid>
     </div>
-  ));
+  );
 };
 
 export default PairingList;
